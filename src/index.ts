c5b4e622230e8e033d29bcce26b1902a5891ac6b
blu3beri <dev@blueberi.nl>
2020-08-26 21:50:18 +0200

    added a few methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+export {};
+
+declare global {
+    interface Array<T> {
+        first(): T;
+        last(): T;
+        max(): number;
+        min(): number;
+        count(i: T): number;
+    }
+}
+
+Array.prototype.first = function () {
+    return this[0];
+}
+
+Array.prototype.last = function () {
+    return this[this.length - 1];
+}
+
+Array.prototype.last = function () {
+    return this[this.length - 1];
+}
+
+Array.prototype.max = function () {
+    return Math.max(...this);
+}
+
+Array.prototype.min = function () {
+    return Math.min(...this);
+}
+
+Array.prototype.count = function (i: any) {
+    return this.filter(item => item === i).length;
+}
